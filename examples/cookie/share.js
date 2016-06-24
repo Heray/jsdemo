240c2f554c726c99584628c6ba8460fc5778cc91
Hearn Yang <heranyang@gmail.com>
2016-06-24 10:43:06 -0400

    ck test

---
+++ examples/cookie/share.js
@@ -0,0 +1,12 @@
+console.log(document.cookie);
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+var url = window.location.href;
+
+setCookie('paul-test', url, 2);
